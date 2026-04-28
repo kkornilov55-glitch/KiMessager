b50d6201a8ec96ad6dbdd9455acda9357877b9ba
Kirill <kkornilov55@gmail.com>
2026-04-28 23:55:55 +0300

    Добавьте файлы проекта.

---
+++ Server/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 6767;
+            TcpListener server = new TcpListener(IPAddress.Any, port);
+
+            try
+            {
+                //1. Запуск сервера
+                server.Start();
+                Console.WriteLine("Сервер запущен... Ожидание подключения");
+
+                while (true)
+                {
+                    //2. Ожидание подключения пользователя, программа на паузе
+                    TcpClient client = server.AcceptTcpClient();
+
+                    //3. После подключения клиента выводим сообщение об этом
+                    Console.WriteLine("\nКлиент подключен!");
+                    Console.WriteLine("-------------------------------------");
+
+                    //4. Открываем поток для общения с конкретным клиентом
+                    NetworkStream stream = client.GetStream();
+
+                    //5. Корзина для полученного сообщения в байтовом представлении
+                    byte[] buffer = new byte[256];
+                    //Принятие сообщения со сбором его в "корзину"
+                    int bufferCount = stream.Read(buffer, 0, buffer.Length);
+
+                    //6. Перевод сообщения из списка байтов в понятный нам текст 
+                    string message = Encoding.UTF8.GetString(buffer, 0, bufferCount);
+                    //Вывод полученного сообщения
+                    Console.WriteLine($"Получено сообщение: {message}");
+
+                    client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+        }
+    }
+}
